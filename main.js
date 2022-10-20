fcebb32d3a0d12c23aa91d9bf8bfe4395bfba558
Gabriel Morato <87160136+gpurplem@users.noreply.github.com>
2022-10-20 19:54:32 -0300

    0.3
    Overflow corrigido.

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-//Quando precisar esperar a página carregar.
-// window.addEventListener('load', function () {
-//     alert("It's loaded!")
-// })
-
-
 /*============================
 Mudar fonte de nav dependendo da página.
 ============================*/
+
 var nomePagina = window.location.pathname;
 var nomePagina = nomePagina.split("/").pop(); //Pega o último elemento do vetor.
 
@@ -17,3 +12,13 @@ if(nomePagina === "index.html"){
     var elem_sobre = document.getElementById("sobre");
     elem_sobre.style.color = "white";
 }
+
+/*============================
+Botão iniciar.
+============================*/
+
+var iniciarBtn = document.getElementById("iniciar-btn");
+
+alerting = () => {alert("teste")};
+
+iniciarBtn.addEventListener('click', alerting);
