bcc2bdf90a9d7c48dbe54dde48f199453d1c8d87
Gabriel Morato <87160136+gpurplem@users.noreply.github.com>
2022-10-20 22:45:27 -0300

    0.5
    Iniciar() e mostrarSelecoes() funcionam.

--- main.js
+++ main.js
@@ -16,9 +16,65 @@ if(nomePagina === "index.html"){
 /*============================
 Botão iniciar.
 ============================*/
+/*============================
+Mostrar seleções (passo 1).
+============================*/
+
+const apiUrl = "https://estagio.geopostenergy.com/WorldCup/GetAllTeams";
+var dados;
+var selecoesArray = new Array(32);
+var dadosHtml = "";
+
+class selecaoObj {
+    constructor(Name, Token) {
+        this.Name = Name;
+        this.Token = Token;
+        this.QtdGols = 0;
+        this.QtdGolsPenalti = 0;
+        this.Pontuacao = 0;
+    }
+}
+
+async function getSelecoes(url) {
+    
+    const resposta = await fetch(url, 
+        {
+            method: 'GET', headers: {'git-user': 'gpurplem'},
+            response: 'JSON' 
+        });
+    
+        dados = await resposta.json();
+
+        for(let i = 0; i<32; i++){
+            selecoesArray[i] = new selecaoObj(dados.Result[i].Name, dados.Result[i].Token);
+        }
+}
+
+async function mostrarSelecoes() {
+    dadosHtml = "<table><tr><th>Seleção</th><th>Gols</th><th>Pênaltis</th><th>Pontuação</th></tr>'";
+
+    for(let i = 0; i<32; i++){
+        let pais = selecoesArray[i]['Name'];
+        let gols = selecoesArray[i]['QtdGols'];
+        let penaltis = selecoesArray[i]['QtdGolsPenalti'];
+        let pontuacao = selecoesArray[i]['Pontuacao'];
+
+        dadosHtml += '<tr>' + '<td>' + pais + '</td>' + '<td>' + gols + '</td>' 
+        + '<td>' + penaltis + '</td>' + '<td>' + pontuacao + '</td>' + '</tr>';
+    }
+    dadosHtml += "</table>";
+
+    document.getElementById("main_body").innerHTML = dadosHtml;
+}
 
 var iniciarBtn = document.getElementById("iniciar-btn");
 
-alerting = () => {alert("teste")};
+async function iniciar() {
+    iniciarBtn.remove();
+    document.getElementById("main_body").className = "index_main_body_inner";
+    await getSelecoes(apiUrl);
+    await mostrarSelecoes();
+};
 
-iniciarBtn.addEventListener('click', alerting);
+iniciarBtn.addEventListener('click', iniciar);
+
